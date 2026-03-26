3b7dd62c22789ac4e412eb6874de82e75ac4d654
Aryan Srivastava <143885879+Sri-Aryan@users.noreply.github.com>
2026-03-26 09:37:39 +0530

    chore: branding

--- lib/main.dart
+++ lib/main.dart
@@ -16,7 +16,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Doer',
+      title: 'Taskify',
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
         useMaterial3: true,
